2e451f9d392dc083c385b6222d0a89493a8fb85d
salamicompiler <salamiwris@gmail.com>
2020-05-08 12:20:54 -0400

    my linuxjobber labs

---
+++ myscrumy/salamiwarisscrumy/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class SalamiwarisscrumyConfig(AppConfig):
+    name = 'salamiwarisscrumy'
---
+++ myscrumy/salamiwarisscrumy/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def get_grading_parameter(request):
+	return HttpResponse("Welcome to Django")
+
